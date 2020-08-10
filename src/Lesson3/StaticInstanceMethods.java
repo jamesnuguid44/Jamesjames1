41883a583e7c87970f4bc2b9256e2be98bfdfceb
jamesjames1 <james44nuguid@gmail.com>
2020-08-10 17:17:37 +0800

    newExercises

---
+++ src/Lesson3/StaticInstanceMethods.java
@@ -0,0 +1,18 @@
+package Lesson3;
+
+public class StaticInstanceMethods {
+
+    static void address() {
+        System.out.println("Pulungdagal");
+        System.out.println("Solib");
+        System.out.println("Pulungdagal");
+
+    }
+    public static void main(String[] args) {
+
+        address();
+        System.out.println("^address^");
+
+
+    }
+}
---
+++ src/LessonExercises/exercise3.java
@@ -0,0 +1,16 @@
+package LessonExercises;
+import java.util.Scanner;
+
+public class exercise3 {
+    public static void main(String[] args) {
+    Scanner num = new Scanner(System.in);
+    System.out.print("Please enter an integer: ");
+    int number = num.nextInt();
+    int numnum = number % 2;
+
+    boolean even = numnum == 0;
+    System.out.println("\nIs "+number+" an even number? "+ even );
+
+    System.out.println("Is "+number+" an odd number? "+ (numnum==1));
+    }
+}
