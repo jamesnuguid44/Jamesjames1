8a948c1318b6274ad0d96e7773a32b2fc1ed1f3e
jamesjames1 <james44nuguid@gmail.com>
2020-09-20 14:08:19 +0800

    Lesson-06-Finished-Exercises

---
+++ src/Lesson6/Exercise2.java
@@ -0,0 +1,23 @@
+package Lesson6;
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        final int MIN = 1;
+        final int MAX = 10;
+
+        System.out.print("Please guess the number: ");
+        int guess = scan.nextInt();
+
+        int random = (int) (Math.random() * MAX) + MIN;
+        System.out.println("The correct answer is: " + random);
+
+        if (guess > random)
+            System.out.println("Your guess is higher!");
+        else if (guess < random)
+            System.out.println("Your guess is lower!");
+        else
+            System.out.println("Your guess is correct!");
+    }
+}
