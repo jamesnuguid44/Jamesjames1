8eb98f0029621b2a2d11bc82b1251242ff2455c4
jamesjames1 <james44nuguid@gmail.com>
2020-11-26 23:52:58 +0800

    Lesson9

---
+++ src/Lesson9/CountByAnything.java
@@ -0,0 +1,26 @@
+package Lesson9;
+import java.util.Scanner;
+
+public class CountByAnything {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numInterval;
+        System.out.print("Please enter the interval of counting: ");
+        numInterval = scanner.nextInt();
+        int numIntervalNew = numInterval;
+        int numInterval10 = numInterval*10;
+
+        for ( ; numIntervalNew <= 500;numIntervalNew+=numInterval ){
+            if (numIntervalNew == numInterval10){
+                System.out.print(" "+ numIntervalNew + "\n");
+                //numIntervalNew+=numInterval;
+                numInterval10+=numInterval10;
+            }
+            else {
+                System.out.print(numIntervalNew+" ");
+                //numIntervalNew+=numInterval;
+            }
+        }
+
+    }
+}
